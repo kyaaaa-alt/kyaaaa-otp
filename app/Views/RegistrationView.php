f2786ac6484acce87aa7540dcaa7bafb1e170bfa
Naufal Azkia <66082449+naufkia@users.noreply.github.com>
2024-04-17 17:52:26 +0700

    update registration

--- app/Views/RegistrationView.php
+++ app/Views/RegistrationView.php
@@ -5,31 +5,38 @@
     <title><?= esc($title) ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" type="image/png" href="/favicon.ico">
+    <link href="<?= base_url('assets/global/') ?>sweetalert2.min.css" rel="stylesheet" type="text/css" />
     <?= view('components/styles') ?>
 </head>
 <body>
     <div class="card">
         <div class="card-header">
-            <h2>Register</h2>
+            <h2>Registration</h2>
         </div>
-        <div class="card-body">
-            <form>
-                <label for="name">Name</label>
-                <input type="text" id="name" name="name" required>
+        <form name="register">
+            <div class="card-body">
+                    <label for="name">Name</label>
+                    <input type="text" id="name" name="name" required>
 
-                <label for="email">Email</label>
-                <input type="email" id="email" name="email" required>
+                    <label for="email">Email</label>
+                    <input type="email" id="email" name="email" required>
 
-                <label for="phone">Username</label>
-                <input type="username" id="username" name="username" required>
+                    <label for="phone">Username</label>
+                    <input type="username" id="username" name="username" required>
 
-                <label for="password">Password</label>
-                <input type="password" id="password" name="password" required>
-            </form>
-        </div>
-        <div class="card-footer">
-            <button>Save</button>
-        </div>
+                    <label for="password">Password</label>
+                    <input type="password" id="password" name="password" required>
+
+            </div>
+            <div class="card-footer">
+                <button type="submit">Register</button>
+            </div>
+        </form>
     </div>
+    <script src="<?= base_url('assets/global/') ?>jquery-3.7.1.min.js"></script>
+    <script src="<?= base_url('assets/global/') ?>sweetalert2.min.js"></script>
+    <script>
+        
+    </script>
 </body>
 </html>
