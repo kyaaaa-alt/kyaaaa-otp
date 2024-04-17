02b9eb0ab863fe19f2ec3bdfd81bd6cca31e1539
Naufal Azkia <66082449+naufkia@users.noreply.github.com>
2024-04-17 17:35:47 +0700

    Controllers

---
+++ app/Controllers/RegistraionController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controllers;
+
+class RegistraionController extends BaseController
+{
+    public function index(): string
+    {
+        return view('RegistrationView');
+    }
+
+    public function submit()
+    {
+        
+    }
+}
--- app/Controllers/VerifyController.php
+++ app/Controllers/VerifyController.php
@@ -6,6 +6,11 @@ class VerifyController extends BaseController
 {
     public function index(): string
     {
-        return view('welcome_message');
+        return view('VerificationView');
+    }
+
+    public function verify()
+    {
+
     }
 }
